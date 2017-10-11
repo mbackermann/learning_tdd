023f2eb040d386e3da964ddab0ce33984047fb9f
Maurício Ackermann <mauricio@agenciaflin.com.br>
2017-10-10 23:03:31 -0300

    Section 2 - Lecture 11

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -1,15 +1,23 @@
 require 'calculator'
 
 describe Calculator do
-  it 'sum method for two numbers' do
-    calc = Calculator.new
-    result = calc.sum(15, 2)
-    expect(result).to eq(17)
-  end
+  context '#sum' do
+    it 'with positive numbers' do
+      calc = Calculator.new
+      result = calc.sum(15, 2)
+      expect(result).to eq(17)
+    end
 
-  it 'use sum method for two numbers with negative values' do
-    calc = Calculator.new
-    result = calc.sum(-7, 5)
-    expect(result).to eq(-2)
+    it ' with negative and positive values' do
+      calc = Calculator.new
+      result = calc.sum(-7, 5)
+      expect(result).to eq(-2)
+    end
+
+    it 'with negative numbers' do
+      calc = Calculator.new
+      result = calc.sum(-7, -3)
+      expect(result).to eq(-10)
+    end
   end
 end
